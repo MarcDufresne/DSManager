aa1f625acfa08fef4794f0ef8fd92686f1675e6c
Marc-Andre Dufresne <marc.andre.dufresne@gmail.com>
2017-12-01 22:45:06 -0500

    - Started new login activity

--- app/src/main/java/net/imatruck/dsmanager/LoginActivity.kt
+++ app/src/main/java/net/imatruck/dsmanager/LoginActivity.kt
@@ -1,14 +1,103 @@
 package net.imatruck.dsmanager
 
+import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
-
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+import net.imatruck.dsmanager.models.AuthLoginBase
+import net.imatruck.dsmanager.network.SynologyAPIHelper
+import net.imatruck.dsmanager.utils.SynologyBaseError
+import retrofit2.Call
+import java.io.IOException
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        /**
+         * - Maybe receive something that says login is expired
+         * - If Login expired or not logged-in, pre-fill what we can, wait for login
+         * - If logged-in and nothing says it's expired, redirect to TaskList
+         */
+
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+
+        val storedUsername = prefs.getString(getString(R.string.pref_key_account), "")
+        val storedApiURL = prefs.getString(getString(R.string.pref_key_server), "")
+
+        editLoginURL.setText(storedApiURL)
+        editLoginUser.setText(storedUsername)
+
+        button.setOnClickListener {
+
+            val editor = prefs.edit()
+            editor.putString(getString(R.string.pref_key_server), editLoginURL.text.toString())
+            editor.putString(getString(R.string.pref_key_account), editLoginUser.text.toString())
+            editor.apply()
+
+            val synologyAPI = SynologyAPIHelper.getSynologyApi(this)
+
+            val authLoginCall = synologyAPI.authLogin(
+                    editLoginUser.text.toString(), editLoginPass.text.toString())
+
+            val loginTask = LoginTask()
+            loginTask.mLoginActivity = this
+            loginTask.execute(authLoginCall)
+        }
+    }
+
+    class LoginTask : AsyncTask<Call<AuthLoginBase>, Void, AuthLoginBase>() {
+
+        var mLoginActivity: LoginActivity? = null
+
+        override fun doInBackground(vararg calls: Call<AuthLoginBase>): AuthLoginBase? {
+            val loginInfo: AuthLoginBase
+            try {
+                loginInfo = calls[0].execute().body()
+            } catch (e: IOException) {
+                e.printStackTrace()
+                return null
+            }
+
+            return loginInfo
+        }
+
+        override fun onPostExecute(result: AuthLoginBase?) {
+            if (result != null) {
+                val success = result.isSuccess
+                var errorMessage = ""
+                if (result.error != null) {
+                    errorMessage = mLoginActivity!!.getString(
+                            SynologyBaseError.getMessageId(result.error.code))
+                }
+
+                if (success) {
+                    val sid = result.sid
+
+                    val prefs = PreferenceManager.getDefaultSharedPreferences(mLoginActivity)
+                    val editor = prefs.edit()
+                    editor.putString(
+                            mLoginActivity!!.getString(R.string.pref_key_sid), sid)
+                    editor.putString(
+                            mLoginActivity!!.getString(R.string.pref_key_sid_header),
+                            "id=" + sid)
+                    editor.apply()
+
+                    mLoginActivity!!.finish()
+                    val intent = Intent(mLoginActivity!!, TaskListActivity::class.java)
+                    mLoginActivity!!.startActivity(intent)
+
+                } else {
+                    Toast.makeText(mLoginActivity, errorMessage, Toast.LENGTH_LONG).show()
+                }
+            }
+            mLoginActivity = null
+        }
 
     }
 
